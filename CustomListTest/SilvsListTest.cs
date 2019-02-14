5f6af9473d34927ffc1a2f375c01870cd00d9a14
Fp38SilSha <faperez38s@gmail.com>
2019-02-14 01:36:38 -0600

    Added a test for Remove to make sure it only removes first instance and not all of them.

--- CustomListTest/SilvsListTest.cs
+++ CustomListTest/SilvsListTest.cs
@@ -124,6 +124,30 @@ namespace CustomListTest
             Assert.AreEqual(2, myList[0]);
 
         }
+        [TestMethod]
+        public void ListRemove_SingleValue_ListAdjustNextValuesAfterRemoveToPrevious()
+        {
+            SilvsList<int> myList = new SilvsList<int>();
+            myList.Add(1);
+            myList.Add(2);
+            myList.Add(3);
+            myList.Remove(2);
+
+            Assert.AreEqual(1, myList[0]);
+            Assert.AreEqual(3, myList[1]);
+
+        }
+        [TestMethod]
+        public void ListRemove_SingleValue_OnlyFirstInstanceRemoved()
+        {
+            SilvsList<int> myList = new SilvsList<int>();
+            myList.Add(5);
+            myList.Add(5);
+            myList.Add(5);
+            myList.Remove(5);
+
+            Assert.AreEqual(5, myList[1]);
+        }
         //[TestMethod]
         //public void ListAdd_CollectionsValue_ValuesAdded()
         //{
